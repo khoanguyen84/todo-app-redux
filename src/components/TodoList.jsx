efa17f524e1cc819289ab8aa90dbe7552faf9516
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-11-09 13:48:55 +0700

    complete templete

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import Todo from "./Todo";
-import { Col, Row } from "antd";
+import { Col, Row, Input, Button, Select, Tag } from "antd";
 
 function TodoList() {
     return (
-        <Row>
-            <Col span={24}>
+        <Row style={{ height: 'calc(100% - 40px)' }}>
+            <Col span={24} style={{ height: 'calc(100% - 40px)', overflowY: 'auto'}}>
                 <Todo name={'Learning ReactJS'} priority={'Hight'} />
                 <Todo name={'Learning NextJS'} priority={'Medium'} />
                 <Todo name={'Learning Piano'} priority={'Low'} />
+            </Col>
+            <Col span={24}>
+                <Input.Group style={{ display: 'flex' }} compact>
+                    <Input />
+                    <Select defaultValue="Medium">
+                        <Select.Option value='Hight' label='Hight'>
+                            <Tag color="red">Hight</Tag>
+                        </Select.Option>
+                        <Select.Option value='Medium' label='Medium'>
+                            <Tag color="blue">Medium</Tag>
+                        </Select.Option>
+                        <Select.Option value='Low' label='Low'>
+                            <Tag color="gray">Low</Tag>
+                        </Select.Option>
+                    </Select>
+                    <Button type="primary">Add</Button>
+                </Input.Group>
             </Col>
         </Row>
     )
